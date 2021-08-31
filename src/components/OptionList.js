37d373259c41eb8d50e0f882047cd6a36ef896d3
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-31 17:01:23 -0300

    Inicia estilização

--- src/components/OptionList.js
+++ src/components/OptionList.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../css/MainSection.css';
+import ConfigIcon from '../icons/config-icon.png';
 
 function OptionList() {
   return (
     <div className="option-list-container">
-     <label className="option-list-button">Administração</label>
+     <label className="option-list-label">
+      <input
+        type="image"
+        src={ConfigIcon}
+        height="15px"
+        width="15px"
+        className="icon-config"
+      />
+       Administração
+     </label>
+
      <button className="option-list-button">Administradores</button>
      <button className="option-list-button">Áreas</button>
      <button className="option-list-button">Locais de Trabalho</button>
